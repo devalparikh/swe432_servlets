0de164fdaf62568c6e3cb64e1f27512368a96786
Deval Parikh <devalpp@gmail.com>
2020-10-02 22:50:02 -0400

    added truth table capabilities

--- src/main/java/servlet/Assignment5.java
+++ src/main/java/servlet/Assignment5.java
@@ -85,8 +85,8 @@ public class Assignment5 extends HttpServlet {
             "                                        <tr onmouseover=\"logic_table.clickedRowIndex=this.rowIndex\">\n" +
             "                                                <td>\n" +
             "                                                        <select type=\"text\" name=\"Boolean1_1[]\" id=\"ops1_1\">\n" +
-            "                                                                <option value=\"true\">True</option>\n" +
-            "                                                                <option value=\"false\">False</option>\n" +
+            "                                                                <option value=\"p\">p</option>\n" +
+            "                                                                <option value=\"q\">q</option>\n" +
             "                                                        </select>\n" +
             "                                                </td>\n" +
             "                                                <td>\n" +
@@ -106,8 +106,8 @@ public class Assignment5 extends HttpServlet {
             "                                                </td>\n" +
             "                                                <td>\n" +
             "                                                        <select type=\"text\" name=\"Boolean2_1[]\" id=\"ops2_1\">\n" +
-            "                                                                <option value=\"true\">True</option>\n" +
-            "                                                                <option value=\"false\">False</option>\n" +
+            "                                                                <option value=\"p\">p</option>\n" +
+            "                                                                <option value=\"q\">q</option>\n" +
             "                                                        </select>\n" +
             "                                                </td>\n" +
             "                                                <td>\n" +
@@ -211,12 +211,13 @@ public class Assignment5 extends HttpServlet {
     // HTML Body from doPost
     private void PrintPostBody(PrintWriter out, Map formData) {
         out.println(topBodyHTML);
-        PrintUserPredicates(out, formData);
+        String[] predItems = PrintUserPredicates(out, formData);
+        printTruthTable(out, predItems);
         out.println(endBodyHTML);
     }
 
     // Helper to generate HTML for user inputted predicates
-    private void PrintUserPredicates(PrintWriter out, Map formData) {
+    private String[] PrintUserPredicates(PrintWriter out, Map formData) {
         String predicateOutputHTML = "\n" +
                 "                        <h1>Final Predicate</h1>\n" +
                 "                        <h3>This is your inputted predicate put together! We are using the symbol" +
@@ -228,6 +229,8 @@ public class Assignment5 extends HttpServlet {
         Set s = formData.entrySet();
         Iterator it = s.iterator();
         int count = 1;
+
+        String[] predItems = new String[formData.size()-1];
 
         while(it.hasNext()){
 
@@ -254,10 +257,13 @@ public class Assignment5 extends HttpServlet {
                     continue;
                 }
                 if(cur_op.contains("and")) {
+                    predItems[count-1] = "and";
                     out.println("& ");
                 } else if(cur_op.contains("xor")) {
+                    predItems[count-1] = "xor";
                     out.println("^ ");
                 } else if(cur_op.contains("or")) {
+                    predItems[count-1] = "or";
                     out.println("| ");
                 } else {
                     if(count % 4 != 0) {
@@ -266,13 +272,154 @@ public class Assignment5 extends HttpServlet {
                 }
 
             } else if(key.contains("Bool")) {
+                predItems[count-1] = value[0];
                 out.println(value[0]+" ");
             }
-
 
             count++;
         }
         out.println("</div>");
+        return predItems;
+    }
+
+    private void printTruthTable(PrintWriter out, String[] predItems) {
+
+        // Give each unique variable ids to be used as indices for boolean values
+        Map<String, Integer> varIds = new HashMap<>();
+
+        int varId = 0;
+        for(int i = 0; i < predItems.length; i+=2) {
+            String curVar = predItems[i];
+            if(!varIds.containsKey(curVar)) {
+                varIds.put(curVar, varId);
+                varId++;
+            }
+        }
+
+
+        int rows = (int) Math.pow(2, varId);
+
+        //  --------
+        // | p | q |  <-- variables
+        //  --------
+
+        // boolVals:
+        //  --------
+        // | 0 | 0 |
+        //  --------
+        // | 0 | 1 |
+        //  --------
+        // | 1 | 0 |
+        //  --------
+        // | 1 | 1 |
+        //  --------
+
+        int[][] boolVals = new int[rows][varId]; // Each Row is the value of bool vars, each col is a boolvar
+
+
+        // Builds truth table (variable side)
+        for (int i=0; i<rows; i++) {
+            for (int j=varId-1; j>=0; j--) {
+                boolVals[i][varId-1 - j] = (i/(int) Math.pow(2, j))%2;
+            }
+        }
+
+        // Prints truth table (variable side)
+//        for(int i = 0; i < boolVals.length; i++) {
+//            for(int j = 0; j < boolVals[i].length; j++) {
+//                System.out.print(boolVals[i][j] + " ");
+//            }
+//            System.out.println("next");
+//        }
+
+        // Build truth table (result side)
+        int[] ttresults = new int[rows];
+
+        // Go through each row (possibilities of values)
+        for(int i = 0; i < boolVals.length; i++) {
+
+            // boolean variable values. Variable IDs represent the index of this array
+            int[] curBoolVarValues = boolVals[i];
+
+            // Go through predicate and calculate result for current array of values
+            int curResult = 0;
+            int prevResult = 0;
+            for(int j = 0; j < predItems.length-2; j += 2) {
+                int tmpResult = 0;
+
+                // First boolean variable is either first predicate item or previously built up value
+                int curBoolVar1;
+                if(j == 0) {
+                    curBoolVar1 = curBoolVarValues[varIds.get(predItems[j])];
+                } else {
+                    curBoolVar1 = prevResult;
+                }
+
+                // Current operation to do between curBoolVar1 and curBoolVar2
+                String curOp = predItems[j+1];
+
+                // Second boolean variable
+                int curBoolVar2 = curBoolVarValues[varIds.get(predItems[j+2])];
+
+
+                // Do actual operation
+                if(curOp.equals("and")) {
+                    tmpResult = curBoolVar1 & curBoolVar2;
+                } else if(curOp.equals("xor")) {
+                    tmpResult = curBoolVar1 ^ curBoolVar2;
+                } else if(curOp.equals("or")) {
+                    tmpResult = curBoolVar1 | curBoolVar2;
+                }
+
+                // Set prevResult to tmpResult to continue operation
+                prevResult = tmpResult;
+            }
+            curResult = prevResult;
+            ttresults[i] = curResult;
+        }
+
+        // Merge two parts of the truth table
+        int[][] truthTable = new int[rows][varId+1];
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < boolVals[i].length; j++) {
+                truthTable[i][j] = boolVals[i][j];
+            }
+            truthTable[i][truthTable[i].length-1] = ttresults[i];
+        }
+
+        // Output table
+        out.println("<table class=\"result-table\">");
+
+        // Title row
+        out.println("<tr>");
+        out.println("<td>row</td>");
+        Set<String> names = new HashSet<>();
+        for(int i = 0; i <  predItems.length; i+=2) {
+            if(!names.contains(predItems[i])) {
+                out.println("<td>" + predItems[i] + "</td>");
+                names.add(predItems[i]);
+            }
+        }
+        out.println("<td>result</td>");
+        out.println("</td>");
+        out.println("</tr>");
+
+        for(int i = 0; i < truthTable.length; i++) {
+            out.println("<tr>");
+            int rownum = i+1; // only used to show on table
+            out.println("<td>" + rownum + "</td>");
+
+            for(int j = 0; j < truthTable[i].length; j++) {
+                out.println("<td>" + truthTable[i][j] + "</td>");
+                System.out.print(truthTable[i][j] + " ");
+            }
+            out.println("</tr>");
+
+            System.out.println("next");
+        }
+
+        out.println("</table>");
+
 
     }
 
